25e446e032a86017b4111f30f2f020b6f7cb523a
Buse <buseggencoglu@gmail.com>
2020-12-26 15:38:54 +0300

    Refactored template foldering

--- RAR/urls.py
+++ RAR/urls.py
@@ -1,23 +1,17 @@
 
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
-    path('car_list/', views.car_list, name='car_list'),
-    path('demo/', views.dashboard_car_list, name='demo'),
+    path('cars/', views.car_list, name='car_list'),
     path('users/', views.users, name='users'),
     path('profile/<slug:pk>', views.profile, name='profile'),
-    path('reservation_list/', views.reservation_list, name='reservation_list'),
+    path('reservations/', views.reservation_list, name='reservation_list'),
     path('contact/', views.contact, name='contact'),
-    path('my_reservations/', views.view_my_reservation_cardealer, name='my_reservations'),
-    path('my_reservations/', views.view_my_reservation_customer, name='my_reservations'),
-    path('index/', views.index, name='index'),
-    path('search_results/', views.search_results, name='search_results'),
-    path('search/', views.search, name='search'),
-    path('create_car/', views.create_car, name='create_car'),
-    path('car_delete/<int:pk>', views.car_delete, name='car_delete'),
-    path('car_update/<int:pk>', views.car_update, name='car_update'),
-
+    path('cardealer/reservations/', views.view_my_reservation_cardealer, name='cardealer_reservations'),
+    path('customer/reservations/', views.view_my_reservation_customer, name='customer_reservations'),
+    path('car/create/', views.create_car, name='create_car'),
+    path('car/delete/<int:pk>', views.car_delete, name='car_delete'),
+    path('car/update/<int:pk>', views.car_update, name='car_update'),
 ]
