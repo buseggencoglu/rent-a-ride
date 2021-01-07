043feb1b1629ee096128754db836e8f4d59b8da9
Ayşe Bayık <ayse.bayik@isik.edu.tr>
2021-01-07 12:34:04 +0300

    create_car ve update_car featureları için image field eklenebilir ve güncellenebilir. Car_list feature için car image görülebilir. Image eklenmeyen carlar içinse default image eklenme özelliği getirildi.

--- RAR/models.py
+++ RAR/models.py
@@ -47,15 +47,8 @@ class Customer(models.Model):
         return cls.objects.filter(user=user)[0]
 
 
-def uploaded_location(instance, filename):
-    return ("%s/%s") % (instance.carName, filename)
-
-
 class Car(models.Model):
-    image = models.ImageField(upload_to=uploaded_location, null=True, blank=True, width_field="width_field",
-                              height_field="height_field")
-    height_field = models.IntegerField(default=0)
-    width_field = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='car_image', null=True, blank=True, default='car_image/default')
     carName = models.CharField(max_length=100, default="")
     model = models.CharField(max_length=100)
     airconditioner = models.BooleanField()
